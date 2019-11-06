7b565566edd077ce802bc2cb5a1de403753c7974
frescocodes <hueydakotah@gmail.com>
2019-11-05 21:31:29 -0500

    schema created

--- data/migrations/20191105195247_cars_table.js
+++ data/migrations/20191105195247_cars_table.js
@@ -1,9 +1,18 @@
 // changes to make
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('cars', function (table) {
+        table.increments(); // creates primary key called id
+        table.enum('VIN', 50).notNullable();
+        table.string('make', 20).notNullable(); // creates a text field called make
+        table.string('model', 50).notNullable();
+        table.ineger('mileage', 1000000).notNullable();
+        table.integer('year', 4);
+        table.string('title', 20);
+        table.timestamps(true, true);
+    })
 };
 
 // how to undo changes
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars')
 };
